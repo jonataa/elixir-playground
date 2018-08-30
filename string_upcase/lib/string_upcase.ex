1e8871b644846b137c75c45358677883e537c7e3
Jonata Weber <jonataa@gmail.com>
2018-08-30 09:59:08 -0300

    mix format

--- string_upcase/lib/string_upcase.ex
+++ string_upcase/lib/string_upcase.ex
@@ -1,10 +1,11 @@
 defmodule StringUpcase do
   def start do
     receive do
-      {str, from} when is_bitstring(str) -> 
+      {str, from} when is_bitstring(str) ->
         send(from, {:ok, String.upcase(str)})
+
       {notstr, from} ->
-        send(from, {:error, inspect notstr})
+        send(from, {:error, inspect(notstr)})
     end
   end
 end
