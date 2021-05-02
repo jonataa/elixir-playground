edcd1217dd17e81e986377ea595562b7c7e45088
Jonata Weber <jonataa@gmail.com>
2021-05-02 11:59:57 -0300

    Fix SyntaxError in the Chat config

--- distributed-task-chat/config/dev.exs
+++ distributed-task-chat/config/dev.exs
@@ -1,3 +1,3 @@
 use Mix.Config
 
-config :chat, remote_supervisor: fn (recipient) -> {Chat.TaskSupervisor, recipient} end)
+config :chat, remote_supervisor: fn (recipient) -> {Chat.TaskSupervisor, recipient} end
